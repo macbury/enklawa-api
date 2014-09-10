0b5b7019af3245eadcd329a3fa4f258010335359
Arkadiusz Buras <macbury@gmail.com>
2014-09-10 14:20:34 +0200

    json date iso8601

--- lib/enklawa/api/episode.rb
+++ lib/enklawa/api/episode.rb
@@ -1,6 +1,6 @@
 require "uri"
 require "net/http"
-
+require "time"
 module Enklawa
   module Api
     class Episode < Struct.new(:id, :name, :description, :mp3, :pub_date, :link, :duration, :image)
@@ -23,7 +23,7 @@ module Enklawa
           name: name,
           description: description,
           mp3: mp3,
-          pub_date: pub_date.to_i,
+          pub_date: pub_date.utc.iso8601,
           link: link,
           duration: duration,
           image: image
